e1787bf2846eaaf23a27a512f52e5b30355bc0e5
rmortes <56652057+rmortes@users.noreply.github.com>
2024-04-09 18:08:36 +0200

    Replaced utteranc.es with giscus

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -7,11 +7,19 @@ const Content: QuartzComponent = ({ fileData, tree }: QuartzComponentProps) => {
   const classString = ["popover-hint", ...classes].join(" ")
   return <>
     <article class={classString}>{content}</article>
-    <script src="https://utteranc.es/client.js"
-      repo="rmortes/rmortes.github.io"
-      issue-term="url"
-      label="utterance 💬"
-      theme="preferred-color-scheme"
+    <script src="https://giscus.app/client.js"
+      data-repo="rmortes/rmortes.github.io"
+      data-repo-id="R_kgDOLrz9Hw"
+      data-category="Comments"
+      data-category-id="DIC_kwDOLrz9H84Cek4B"
+      data-mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="top"
+      data-theme="noborder_dark"
+      data-lang="es"
+      data-loading="lazy"
       crossorigin="anonymous"
       async>
     </script>
